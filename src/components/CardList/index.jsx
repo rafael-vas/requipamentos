380c56efa4552e6fb80f4d72cbbf32b995ebf9ba
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2024-03-31 16:37:22 -0300

    create cardlist component

---
+++ src/components/CardList/index.jsx
@@ -0,0 +1,65 @@
+import { Card } from "../Card";
+
+import "./style.css";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { ToastContainer } from "react-toastify";
+
+export function CardList({ list, swiperOn, highlight, onClick }) {
+  return (
+    <>
+      {swiperOn ? (
+        <Swiper
+          slidesPerView={window.innerWidth < 500 ? 2 : 4}
+          spaceBetween={window.innerWidth < 500 ? 10 : 20}
+          pagination={{ clickable: true }}
+          navigation
+        >
+          {list.map(({ id, name, code, description, categoria, imgSrc }) => {
+            return (
+              <SwiperSlide key={id + code}>
+                <Card
+                  onClick={onClick}
+                  highlight={highlight}
+                  key={id}
+                  id={id}
+                  name={name}
+                  code={code}
+                  description={description}
+                  categoria={categoria}
+                  imgSrc={imgSrc}
+                />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      ) : (
+        <ul className="card-list">
+          {list.map(({ id, name, code, description, categoria, imgSrc }) => {
+            return (
+              <Card
+                onClick={onClick}
+                key={id}
+                id={id}
+                name={name}
+                code={code}
+                description={description}
+                categoria={categoria}
+                imgSrc={imgSrc}
+              />
+            );
+          })}
+        </ul>
+      )}
+
+      <ToastContainer
+        position="bottom-right"
+        draggable
+        closeOnClick
+        newestOnTop={false}
+        progressClassName="toast-bar"
+      />
+    </>
+  );
+}
