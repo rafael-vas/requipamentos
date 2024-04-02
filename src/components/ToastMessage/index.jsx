bb60ddcee0d0dcde0e5936bb7e0055fd91b3ce91
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2024-04-02 18:47:44 -0300

    create toast-message component

---
+++ src/components/ToastMessage/index.jsx
@@ -0,0 +1,32 @@
+import "./style.css";
+
+import { Flex, Button } from "@radix-ui/themes";
+
+import { ShoppingCartIcon } from "lucide-react";
+
+export function ToastMessage(itemName, itemCode, itemImgSrc) {
+
+  return (
+    <div className="toast">
+      <Flex gap="2" align={"center"}>
+        <img className="toast-img" src={itemImgSrc} />
+        <div className="toast-info">
+          {/* <p className="toast-name">
+            <strong>{itemName}</strong>
+          </p> */}
+          {/* <p className="toast-code">{itemCode}</p> */}
+          <p> foi adicionado ao carrinho</p>
+        </div>
+      </Flex>
+      <Flex>
+        <a className="toast-link" href="/meu-carrinho">
+          <Button className="toast-btn" variant="solid" color="orange">
+            Ver Carrinho <ShoppingCartIcon className="toast-icon" size={16} />
+          </Button>
+        </a>
+      </Flex>
+      {/* <button>Retry</button> */}
+      {/* <button onClick={closeToast}>Close</button> */}
+    </div>
+  );
+}
