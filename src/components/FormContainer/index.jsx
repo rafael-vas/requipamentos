73e7c9438f40fcad1dd0fc1674be4d6d360a6d71
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2024-04-03 19:04:01 -0300

    create form-container component

---
+++ src/components/FormContainer/index.jsx
@@ -0,0 +1,17 @@
+import * as Form from "@radix-ui/react-form";
+import "./style.css";
+
+import { LoaderCircle } from "lucide-react"
+
+export function FormContainer({ children, submitLabel, onSubmit, isLoading }) {
+  return (
+    <Form.Root onSubmit={onSubmit} className="FormRoot">
+      {children}
+      <Form.Submit asChild>
+        <button disabled={isLoading} className="Button" style={{ marginTop: 10 }}>
+          {isLoading ? <LoaderCircle className="loader-icon" /> : submitLabel }
+        </button>
+      </Form.Submit>
+    </Form.Root>
+  );
+}
