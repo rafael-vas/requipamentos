cb818a06bc98198774cec0f0161694cdb3183b02
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2024-04-03 19:03:22 -0300

    create table-container component

---
+++ src/components/TableContainer/index.jsx
@@ -0,0 +1,26 @@
+import "./style.css";
+
+import { Table } from "@radix-ui/themes";
+
+const cartTableHeaders = ["Produto", "Quantidade", "Fabricante"];
+
+export function TableContainer({ tableHeaders = cartTableHeaders, children }) {
+  return (
+    <Table.Root variant="surface" size={1}>
+      <Table.Header>
+        <Table.Row>
+          {tableHeaders.map((tableHeader, i) => {
+            return (
+              <Table.ColumnHeaderCell key={i + tableHeader}>
+                {tableHeader}
+              </Table.ColumnHeaderCell>
+            );
+          })}
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {children}
+      </Table.Body>
+    </Table.Root>
+  );
+}
