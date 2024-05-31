3622a2e28eacbbf516de17ebd741fce33943040f
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2024-05-31 16:44:41 -0300

    delete logo-text & change logo-img

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,12 +1,12 @@
 import "./style.css";
 
-import logoImg from '../../assets/logo.png'
+import logoImg from '/logo.png'
 
 export function Logo() {
     return (
         <a href="/" className="logo-link">
             <img className="logo-img" src={logoImg} alt="logo" />
-            <h1 className="logo-txt"><span className="highlight">re</span>quipamentos</h1>
+            {/* <h1 className="logo-txt"><span className="highlight">re</span>quipamentos</h1> */}
         </a>
     )
 }
