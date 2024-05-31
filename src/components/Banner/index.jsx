4bf47ac3f1306b282fe2be141e89cbfbb78810b0
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2024-05-31 16:43:58 -0300

    import desktop & mobile banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -10,32 +10,46 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
-import banner from "../../assets/banner-orcamento.jpg";
+import bannerDesktop from "../../assets/banner-1-desktop.jpg";
+import bannerMobile from "../../assets/banner-1-mobile.jpg";
 
 export function Banner() {
   return (
     <>
       <Swiper slidesPerView={1} pagination={{ clickable: true }} navigation>
         <SwiperSlide>
+        <picture>
+          <source media="(max-width: 768px)" srcSet={bannerMobile} />
+          <source media="(min-width: 769px)" srcSet={bannerDesktop} />
           <img
             className="slide-item"
-            src={banner}
-            alt=""
+            src={bannerDesktop}
+            alt="Imagem"
           />
+        </picture>
+
         </SwiperSlide>
         <SwiperSlide>
+        <picture>
+          <source media="(max-width: 768px)" srcSet={bannerMobile} />
+          <source media="(min-width: 769px)" srcSet={bannerDesktop} />
           <img
             className="slide-item"
-            src={banner}
-            alt=""
+            src={bannerDesktop}
+            alt="Imagem"
           />
+        </picture>
         </SwiperSlide>
         <SwiperSlide>
+        <picture>
+          <source media="(max-width: 768px)" srcSet={bannerMobile} />
+          <source media="(min-width: 769px)" srcSet={bannerDesktop} />
           <img
             className="slide-item"
-            src={banner}
-            alt=""
+            src={bannerDesktop}
+            alt="Imagem"
           />
+        </picture>
         </SwiperSlide>
       </Swiper>
     </>
